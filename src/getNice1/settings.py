2aabb18a34a6fec6a9a3212c9c7cf11d23595090
laurenelle <lauren.elle.t@gmail.com>
2014-07-29 18:06:15 -0700

    reverts to paypal default button and modifies db

--- src/getNice1/settings.py
+++ src/getNice1/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'signups',
+
     )
 
 MIDDLEWARE_CLASSES = (
